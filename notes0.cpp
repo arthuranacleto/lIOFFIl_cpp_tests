d2ef9744baa4c2fa856cedc6e9550f1113d29b01
Arthur Anacleto <60738221+arthuranacleto@users.noreply.github.com>
2020-05-28 06:58:01 -0300

    up2-85

---
+++ notes0.cpp
@@ -0,0 +1,31 @@
+/[] - Você pode chamar os comandos cout <<, cin >>, e suas classes desse modo:
+
+std :: cout << "Exemplo";
+
+std :: cin >> Exe;
+
+Mas se você declarar a linha using namespace std; você ganhará tempo escrevendo apenas assim:
+
+cout << "Exemplo";
+
+cin >> Exe; - []/
+
+
+#include <iostrem>
+using namespace std;
+
+int main () {
+
+	//Variáveis:
+		int vidas; //10,25,1000
+	~	char letra; //'B'
+	~~ 	char letras[20]; // essa declaração passar a ser um VETOR. eu posso colocar um texto de até 20 caracteres..
+		double decimal 1; //2.499999 
+		float decimal 2; //2.5
+		bool vivo; //(recebe verdadeiro ou falso, então vai ser representado com o valor true or false
+	~	string nome; //recebe texto "Arthur"
+
+	system("pause");
+	return 0
+	
+	}
---
+++ ola_mundo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+	using namespace std; //no resto do meu código o compilador deve considerar q tô usando o namespace std, p/ ñ precisar colocar um std:: antes, std::cout, std::cin...
+#include <stdlib.h>
+#include <stdio.h>
+
+int main () {
+	cout << "Olá, Mundo!" <<endl; //printf("Olá, Mundo! \n ");
+	cout << "#partiu\nBora codar" <<endl;
+
+	system ("pause");	
+	return 0;
+}
